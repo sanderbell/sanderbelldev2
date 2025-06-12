2f4295973ebf74481c800a13ce8346e9711aba92
Sander Bell <sander@ling-app.com>
2025-06-12 17:46:04 +0700

    Add Oxygen Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+ extend: {
+      fontFamily: {
+        oxygen: ['Oxygen', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
