4935f0686c07fdc7eff4b3b25d2e1626bf536d9c
Sander Bell <thesanderbell@gmail.com>
2026-01-06 17:42:43 +0700

    Add bg-clip-text class to headers for improved text visibility

--- src/App.tsx
+++ src/App.tsx
@@ -200,11 +200,11 @@ function App() {
       <div className="max-w-6xl mx-auto fade-in">
         <div className="text-center my-12">
           <div className="p-6 mb-6">
-            <h1 className="text-4xl sm:text-6xl md:text-8xl font-black bg-gradient-to-r from-blue-600 to-purple-500 text-transparent">
+            <h1 className="text-4xl sm:text-6xl md:text-8xl font-black bg-gradient-to-r from-blue-600 to-purple-500 text-transparent bg-clip-text">
               Product Manager.{' '}
             </h1>
             <br />
-            <h2 className="text-3xl sm:text-5xl md:text-7xl font-black bg-gradient-to-r from-blue-600 to-purple-500 text-transparent">
+            <h2 className="text-3xl sm:text-5xl md:text-7xl font-black bg-gradient-to-r from-blue-600 to-purple-500 text-transparent bg-clip-text">
               Mobile & Technical.{' '}
             </h2>
           </div>
