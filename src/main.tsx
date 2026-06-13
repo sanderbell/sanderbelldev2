be7c4da65fc5286839bde36626a1b2165e2dbc6d
Sander Bell <thesanderbell@gmail.com>
2026-06-13 13:55:05 +0700

    Clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import Launchpal from './Launchpal.tsx'
 import LaunchpalLanding from './launchpal/LaunchpalLanding.tsx'
 import LaunchpalStart from './launchpal/LaunchpalStart.tsx'
 import LaunchpalTos from './launchpal/LaunchpalTos.tsx'
@@ -16,8 +15,10 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/launchpal" element={<LaunchpalLanding />} />
         <Route path="/launchpal/start" element={<LaunchpalStart />} />
         <Route path="/launchpal/tos" element={<LaunchpalTos />} />
-        {/* Legacy route kept for backwards compat */}
-        <Route path="/launchpal-old" element={<Launchpal />} />
+        <Route
+          path="/launchpal-old"
+          element={<Navigate to="/launchpal" replace />}
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
